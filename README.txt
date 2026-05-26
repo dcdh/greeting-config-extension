f1d000f6b823cd1c11c645e61eeb0153f2a43c41
damien <damien.clementdhuart@gmail.com>
2026-05-26 08:18:31 +0200

    ok when using test profile

--- README.txt
+++ README.txt
@@ -1,1 +1,3 @@
 How to fix https://github.com/quarkusio/quarkus/discussions/47981
+
+Not a probleme when using a test profile. See the branch `test-profile-is-ok`.
--- integration-tests/src/test/java/com/damdamdeo/greeting/config/extension/it/GreetingConfigExtensionResourceTest.java
+++ integration-tests/src/test/java/com/damdamdeo/greeting/config/extension/it/GreetingConfigExtensionResourceTest.java
@@ -3,12 +3,23 @@ package com.damdamdeo.greeting.config.extension.it;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
+@TestProfile(GreetingConfigExtensionResourceTest.CustomTestProfile.class)
 public class GreetingConfigExtensionResourceTest {
+
+    public static class CustomTestProfile implements QuarkusTestProfile {
+
+        @Override
+        public String getConfigProfile() {
+            return "custom";
+        }
+    }
 
     @Test
     public void testHelloEndpoint() {
@@ -16,6 +27,6 @@ public class GreetingConfigExtensionResourceTest {
                 .when().get("/greeting-config-extension")
                 .then()
                 .statusCode(200)
-                .body(is("Hello greeting-config-extension false"));
+                .body(is("Hello greeting-config-extension true"));
     }
 }
